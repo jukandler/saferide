74634a1e096b3e2a4a8a1f6131bebca7c0bc9eff
Julia Kandler <julia.kandler@sz.de>
2022-11-27 18:30:34 +0100

    add scrollToTop

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ import Home from './pages/Home';
 
 import './App.css';
 import Imprint from "./pages/Imprint";
+import ScrollToTop from "./helpers/scrollToTopl";
+import Portfolio from "./pages/Portfolio";
+import NavBar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <HashRouter basename={process.env.PUBLIC_URL}>
+        <ScrollToTop />
+        <NavBar />
         <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/impressum" element={<Imprint/>} />
+          <Route exact path="/leistungen" element={<Portfolio/>} />
         </Routes>
       </HashRouter>
     </div>
