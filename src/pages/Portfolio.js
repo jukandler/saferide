9e56d8c5b255d84af24aa5ce9de59e58e9f115c7
Julia Kandler <julia.kandler@sz.de>
2022-11-27 18:30:13 +0100

    add imprint and portfolio

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,53 @@
+import Footer from '../components/footer/Footer';
+import Header from '../components/header/Header';
+
+function Portfolio() {
+    return (
+        <div id="portfolio">
+            <Header />
+            <div className="grid-text_wide">
+                <h1>Leistungen</h1>
+                <div className="disclaimer">
+                    <p><b>Hinweis: Die genauen Trainingsinhalte werden immer auf die individuellen Bedürfnisse des Kunden abgestimmt.</b></p>
+                    <p>Bei den unten aufgeführten Punkten handelt es sich um mögliche Schwerpunkte.</p>
+                    <p>Auf Wunsch sind auch Trainings in Kleingruppen oder Roller-Trainings möglich.</p>
+                </div>
+                <h2>Auto Fahrtraining</h2>
+                <section>
+                    <p>Auffrischungsstunden</p>
+                    <p>Wiedereinstieg nach längerer Fahrpause</p>
+                    <p>Sicheres Fahren im Alter</p>
+                    <p>Fahrängste nach Unfall</p>
+                    <p>Unterstützung bei Blockaden oder Panikattacken</p>
+                </section>
+                <section>
+                    <p>Fahren mit körperlichen Einschränkungen</p>
+                    <p>Beobachtungsfahrt nach Erkrankungen</p>
+                </section>
+                <section>
+                    <p>Wohnmobil, Wohnwagen und Anhänger-Schulung</p>
+                    <p>Fahrtechnik und Notmanöver</p>
+                    <p>Neues Fahrzeug / Assistenzsysteme kennenlernen</p>
+                </section>
+                <h2>Motorrad Fahrtraining</h2>
+                <section>
+                    <p>Auffrischungsstunden</p>
+                    <p>Wiedereinstieg nach längerer Fahrpause</p>
+                    <p>Tipps zum Führerschein für Späteinsteiger</p>
+                </section>
+                <section>
+                    <p>Beratung bei der Wahl des richtigen Motorrads</p>
+                    <p>Neues Fahrzeug kennenlernen</p>
+                </section>
+                <section>
+                    <p>Verbesserung der Fahrtechnik</p>
+                    <p>Notmanöver</p>
+                    <p>Sicherheit in Kurven und Schräglage</p>
+                </section>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Portfolio;
