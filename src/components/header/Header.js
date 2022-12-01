d672977d3c012e0f46cf452938507296b37e5e92
Julia Kandler <julia.kandler@sz.de>
2022-12-01 19:11:57 +0100

    add header image for about page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,9 @@
 import './header.css';
 
-function Header({ title, image = "https://images.unsplash.com/photo-1596649714492-a8f90ecb3776?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3570&q=80" }) {
+function Header({ title, imageWidth, image = "https://images.unsplash.com/photo-1596649714492-a8f90ecb3776?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3570&q=80" }) {
 
     return (
-        <header>
+        <header class={`${imageWidth ? `image-width-${imageWidth}` : ""}`}>
             <img className="hero-image" src={image} alt="Fahrtraining im Auto" />
             <div className="header-content grid-text_wide">
                 <h1>{title}</h1>
--- src/pages/About.js
+++ src/pages/About.js
@@ -8,11 +8,10 @@ import stefanPortrait from '../assets/stefan_portrait.jpeg';
 function About() {
     return (
         <div id="about">
-            <Header title="Dein Fahrtrainer" image="https://images.unsplash.com/photo-1553782097-130fef5d3e27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3456&q=80"/>
+            <Header title="Dein Fahrtrainer" image={stefanPortrait} imageWidth="limited"/>
             <div className="grid-text_wide">
                 <h2>Hallo, ich bin Stefan!</h2>
                 <section  className="introduction image-left">
-                    <img className="portrait-image" src={stefanPortrait} alt="portrait fahrlehrer"></img>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </section>
                 <section  className="introduction image-right">
