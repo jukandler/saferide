7ef26f1bf73933088afd487f7b68ac488b6df4c2
Julia Kandler <julia.kandler@sz.de>
2023-09-15 13:54:42 +0200

    fix attribute name

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import './header.css';
 function Header({ title, imageWidth, image }) {
 
     return (
-        <header class={`${imageWidth ? `image-width-${imageWidth}` : ""} ${image ? "" : "minimal"}`}>
+        <header className={`${imageWidth ? `image-width-${imageWidth}` : ""} ${image ? "" : "minimal"}`}>
             { image && <img className="hero-image" src={image} alt="Fahrtraining im Auto" />}
             <div className="header-content grid-text_wide">
                 <h1>{title}</h1>
