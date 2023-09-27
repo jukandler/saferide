287645b0da13fc107d84ed48ac0fb06bbfcfcedd
Julia Kandler <julia.kandler@sz.de>
2023-09-27 11:11:21 +0200

    add ffc voucher

--- src/pages/Vouchers.js
+++ src/pages/Vouchers.js
@@ -3,6 +3,7 @@ import heroVouchers from '../assets/hero_vouchers.jpg';
 
 import voucherCar from '../assets/gutschein_auto.jpg';
 import voucherBike from '../assets/gutschein_motorrad.jpg';
+import voucherffc from '../assets/gutschein_ffc.jpg';
 
 function Vouchers() {
     return (
@@ -19,6 +20,15 @@ function Vouchers() {
             <div className="grid-expanded">
                     <img src={voucherCar} alt="Gutschein Auto Fahrtraining" />
                     <img src={voucherBike} alt="Gutschein Motorrad Fahrtraining" />
+            </div>
+            <div className="grid-text_wide">
+                <section>
+                    <h2>ADAC Fahr-Fitness-Check</h2>
+                    <p>Auch für den ADAC Fahr-Fitness-Check können Gutscheine ausgestellt werden:</p>
+                </section>
+            </div>
+            <div className="grid-text_wide">
+                    <img src={voucherffc} alt="Gutschein ADAC Fahr-Fitness-Check" />
             </div>
         </div>
     )
