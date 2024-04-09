ca158e3494d41664aab6495466cb95b8a11d24d6
Julia Kandler <julia.kandler@sz.de>
2024-04-09 21:19:13 +0200

    update packages, add media sources

--- src/pages/Fitness.jsx
+++ src/pages/Fitness.jsx
@@ -1,6 +1,9 @@
 import Header from '../components/header/Header';
 import heroCoaching from '../assets/hero_fitness.jpg';
-import poster from '../assets/poster.jpg';
+import posterSat1 from '../assets/posterSat1.jpg';
+import posterBR from '../assets/posterBR.jpg';
+import berichtLT from '../assets/berichtLT.pdf';
+import posterLT from '../assets/berichtLT.jpg';
 
 function Fitness() {
 
@@ -16,13 +19,21 @@ function Fitness() {
                         <p>
                             Der Fahr-Fitness-Check ist ein speziell vom ADAC konzipiertes Programm, das bei mir als Trainer absolviert werden kann. Das Programm richtet sich an ältere, noch regelmäßig Auto fahrende Personen, die zusammen mit speziell durch den ADAC qualifizierten Fahrlehrinnen und Fahrlehrern ihre momentanen persönlichen Fahrfertigkeiten bewerten möchten - kompetent, neutral und ohne Risiko für den Führerschein.
                         </p>
-                        <h3>Video zum Fahr-Fitness-Check</h3>
-                        <p>
-                            In folgendem Video wird der ADAC Fahr-Fitness-Check präsentiert, welcher in Zusammenarbeit zwischen SAT.1, dem ADAC Südbayern und mir als zertifiziertem ADAC Fahr-Fitness-Check-Trainer entstanden ist:
-                        </p>
+                        <h3>Bayrischer Rundfunk (März 2024)</h3>
+                        <p>Senioren am Steuer: Ist ein Medizin-Check sinnvoll?</p>
+                        <a href="https://www.ardmediathek.de/video/gesundheit/senioren-am-steuer-ist-ein-medizin-check-sinnvoll/br-fernsehen/Y3JpZDovL2JyLmRlL3ZpZGVvLzUyZWRkMjE0LWE2YTYtNDQ4Ni04NTk2LTdiNzc5Mjk0NmI1Mg" rel="noopener noreferrer" target="_blank" className="video-preview">
+                            <div className="icon-play"></div>
+                            <img src={posterBR} alt="Bayrischer Rundfunk Video Fahr-Fitness-Check"></img>
+                        </a>
+                        <h3>SAT.1 (August 2023)</h3>
+                        <p>On Tour! Auto fahren im Alter? Das taugt ein Fahr-Fitness-Check</p>
                         <a href="https://www.sat1.de/serien/1730-sat1-bayern/videos/on-tour-auto-fahren-im-alter-das-taugt-ein-fahr-fitness-check" rel="noopener noreferrer" target="_blank" className="video-preview">
                             <div className="icon-play"></div>
-                            <img src={poster} alt="Vorschaubild zum Video Fahr-Fitness-Check"></img>
+                            <img src={posterSat1} alt="Vorschaubild zum Video Fahr-Fitness-Check"></img>
+                        </a>
+                        <h3>Bericht im Landsberger Tagblatt (Februar 2024 )</h3>
+                        <a href={berichtLT} target="_blank" rel="noopener noreferrer">
+                            <img src={posterLT} alt="Zeitungsbericht aus dem Landsberger Tagblatt"></img>
                         </a>
                         <h3>Flyer</h3>
                         <p>
